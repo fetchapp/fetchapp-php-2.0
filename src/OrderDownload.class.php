f22d8ca1050e8a57c97ad51c250065fdb94bb163
Brendon Dugan <wishingforayer@gmail.com>
2013-06-01 13:27:42 -0400

    Created stubs for all of the basic data structures. More classes will be added later, but this should represent all of the classes that someone using the library will interact with.

---
+++ src/OrderDownload.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Brendon Dugan <wishingforayer@gmail.com>
+ * Date: 6/1/13
+ * Time: 1:24 PM
+ */
+
+namespace FetchApp\API;
+
+
+class OrderDownload {
+
+}
