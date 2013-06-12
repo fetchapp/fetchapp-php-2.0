c69c233a65d203bedb1058363136fc7015cbe0d8
Brendon Dugan <wishingforayer@gmail.com>
2013-06-12 16:17:47 -0400

    Implemented stubs for all main FetchApp class functionality.

--- src/FetchApp.class.php
+++ src/FetchApp.class.php
@@ -9,6 +9,98 @@
 namespace FetchApp\API;
 
 
-class FetchApp {
+class FetchApp
+{
+
+    /**
+     * @var $AuthenticationKey String
+     */
+    private $AuthenticationKey;
+
+    /**
+     * @var $AuthenticationToken String
+     */
+    private $AuthenticationToken;
+
+    function __construct()
+    {
+    }
+
+    /**
+     * @param String $AuthenticationKey
+     */
+    public function setAuthenticationKey($AuthenticationKey)
+    {
+        $this->AuthenticationKey = $AuthenticationKey;
+    }
+
+    /**
+     * @return String
+     */
+    public function getAuthenticationKey()
+    {
+        return $this->AuthenticationKey;
+    }
+
+    /**
+     * @param String $AuthenticationToken
+     */
+    public function setAuthenticationToken($AuthenticationToken)
+    {
+        $this->AuthenticationToken = $AuthenticationToken;
+    }
+
+    /**
+     * @return String
+     */
+    public function getAuthenticationToken()
+    {
+        return $this->AuthenticationToken;
+    }
+
+    /**
+     * @return AccountDetail
+     */
+    public function getAccountDetails()
+    {
+        $this->verifyReadiness();
+        $detail = new AccountDetail();
+
+        return $detail;
+    }
+
+    /**
+     * @param int $status
+     * @param $itemsPerPage
+     * @param $pageNumber
+     * @return Order[]
+     */
+    public function getOrders($status = OrderStatus::All, $itemsPerPage = -1, $pageNumber = -1)
+    {
+        $orders = array();
+
+        return $orders;
+    }
+
+    /**
+     * @return OrderDownload[]
+     */
+    public function getDownloads()
+    {
+        $downloads = array();
+
+        return $downloads;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private function verifyReadiness()
+    {
+        if (empty($this->AuthenticationKey) || empty($this->AuthenticationToken)) {
+            throw new \Exception("You must configure an Authentication Key and an Authentication Token before you can connect to FetchApp.");
+        }
+    }
+
 
 }
